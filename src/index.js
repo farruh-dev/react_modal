b616eb357920fe729a643cf3a21e8ad496ada430
Farruh Olimov <farruhbekolimov3904@gmail.com>
2021-11-22 13:45:21 +0500

    start app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import reactDom from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const text = <h1>Hello world!!!</h1>
+const text2 = <h1>Hello react!!!</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const rootDiv = document.querySelector("#root")
+
+reactDom.render(
+  <>
+  {text}
+  {text2}
+  </>
+  , rootDiv)
